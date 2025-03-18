a73b7f44c1c4ced60423f6d3532b1c9b1f1f06c6
pavan-900 <pavanshankar9000@gmail.com>
2025-03-18 21:06:49 +0530

    Update mongo_connection.py

--- mongo_connection.py
+++ mongo_connection.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 import gridfs
 
 # ✅ MongoDB Atlas Connection
-client = MongoClient("mongodb+srv://pavanshankar9000:pavan%409000@project1.gfku5.mongodb.net/?retryWrites=true&w=majority")
-db = client["test6_db"]
+client = MongoClient("mongodb+srv://pavanshankar:pavan%4096188@cluster0.mns8h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["Finish_db"]
 fs = gridfs.GridFS(db)
